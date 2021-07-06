c2c138df9f72b131ea1c34d9cd4fde8ef3821e10
islamnashaat03 <islamnashaat03@gmail.com>
2021-07-06 23:56:02 +0200

    footeer

--- content/js/main.js
+++ content/js/main.js
@@ -1,4 +1,4 @@
-
+//  START TIMER IN HOME PAGE 
 var countDownDate = new Date("Jul 14, 2021 12:00:00").getTime(),
 
 myfunc = setInterval(function() {
@@ -24,11 +24,46 @@ document.getElementById("secs").innerHTML = ""
 document.getElementById("end").innerHTML = "TIME UP!!";
 }
 }, 1000);
-
-$(function(){
-    "use strict"
-    $(".course-published__count").countTo();
+// END TIMER IN HOME PAGE 
+// START COUNTER IN HOME PAGE 
+// $(function(){
+//     "use strict"
+//     $(".course-published__count").countTo();
     
-});
+// });
+function countUp() {
+    $('.course-published__count').each(function() {
+        $(this).prop('Counter', 0).animate({
+          Counter: $(this).text()
+        }, {
+          duration: 4000,
+          easing: 'swing',
+          step: function(now) {
+            $(this).text(Math.ceil(now));
+          }
+        });
+      });
+    }
+    $(function() {
+      "user strict";
+      var bAnimate = true;
+      $(".course-published__count").css ("opacity", "0.0");
+      
+      $(window).scroll(function() {
+        console.log("scroll top=" + $(this).scrollTop());
+        console.log("div offset top=" + $("div").offset().top);
+        var scrolling = $(this).scrollTop(),
+          divoffset = $(".course-published__count").offset().top,
+          screenBottom = scrolling + $(window).height(), 
+          elemBottom = divoffset + $(".course-published__count").outerHeight (); // 
+        if (screenBottom > elemBottom) {
+          if (bAnimate) {
+             $(".course-published__count").css ("opacity", "1.0");
+            countUp();
+            bAnimate = false;
+          }
+        }
+      })
+    })
 
 
